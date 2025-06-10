e3ab56fb36e3232cd506a97de3b979499fde1469
monogo-001 <monogo-001@monogo-001s-MacBook-Pro.local>
2025-06-10 21:40:00 +0200

    BN-01-Add HeroSection, header and button component UI

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,3 +1,46 @@
+<script lang="ts">
+  import type { Snippet } from 'svelte';
+
+  interface BasicProps {
+    children: Snippet;
+    isSecondary?: boolean;
+    isDanger?: boolean;
+    isMenu?: boolean;
+  }
+
+  interface ButtonProps extends BasicProps {
+    onclick?: (e: MouseEvent) => void;
+    href?: never;
+    type?: 'submit' | 'button';
+  }
+
+  interface LinkProps extends BasicProps {
+    href: string;
+    onclick?: never;
+  }
+
+  type ComponentProps = ButtonProps | LinkProps;
+
+  let { children, href, onclick, isSecondary, isDanger, isMenu, ...props }: ComponentProps = $props();
+</script>
+
+{#if href}
+  <a {href} class="btn" class:btn-secondary={isSecondary} class:btn-danger={isDanger} class:btn-menu={isMenu}>
+    {@render children()}
+  </a>
+{:else}
+  <button
+    {...props}
+    {onclick}
+    class="btn"
+    class:btn-secondary={isSecondary}
+    class:btn-danger={isDanger}
+    class:btn-menu={isMenu}
+  >
+    {@render children()}
+  </button>
+{/if}
+
 <style>
   a {
     display: block;
