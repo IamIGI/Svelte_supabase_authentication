35d962719fc842a6858da171ab0e6629fda9b37d
Igor Klusek <igor.klusek@non.se.com>
2025-06-11 20:49:06 +0200

    BN-03-Add authentication with supabase && add login and register page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,20 @@
-<script lang="ts">
+<script>
+  import { invalidate } from '$app/navigation';
   import { Header } from '$components';
   import './../app.css';
 
-  let { children } = $props();
+  let { data, children } = $props();
+  let { session, supabase, user } = $derived(data);
+
+  $effect(() => {
+    const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
+      if (newSession?.expires_at !== session?.expires_at) {
+        invalidate('supabase:auth');
+      }
+    });
+
+    return () => data.subscription.unsubscribe();
+  });
 </script>
 
 <Header />
