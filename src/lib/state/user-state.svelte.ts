ce67dc1362ccd246f0ea5374302a8bc1c3033337
Igor Klusek <igor.klusek@non.se.com>
2025-06-19 21:44:08 +0200

    BN-10-Add edit_mode && add updateBooks database

--- src/lib/state/user-state.svelte.ts
+++ src/lib/state/user-state.svelte.ts
@@ -5,6 +5,8 @@ import { getContext, setContext } from "svelte";
 
 export type Book = Database['public']['Tables']['books']['Row']
 
+type UpdatableBookFields = Omit<Book, "id" | "user_id" | "created_at">;
+
 interface UserStateProps {
     session: Session | null;
     supabase: SupabaseClient | null;
@@ -97,14 +99,33 @@ export class UserState {
         const mostCommonGenre = this.getFavoriteGenre();
     
         return this.allBooks
-          .filter((book) => book.genre?.includes(mostCommonGenre))
-          .toSorted((a, z) => {
+        .filter((book) => book.genre?.includes(mostCommonGenre))
+        .toSorted((a, z) => {
             const ratingA = a.rating || 0;
             const ratingZ = z.rating || 0;
             return ratingZ - ratingA;
-          });
-      }
+        });
+    }
+
+    getBookById(bookId: number) {
+        return this.allBooks.find((book) => book.id === bookId);
+    }
     
+    async updateBook(bookId: number, updateObject: Partial<UpdatableBookFields>) {
+        if(!this.supabase) return;
+
+        const {status, error} = await this.supabase.from("books").update(updateObject).eq("id", bookId);
+
+        if(status == 204 && !error) {
+            this.allBooks = this.allBooks.map((book) => {
+                if(book.id === bookId) {
+                    return {
+                        ...book, ...updateObject
+                    };
+                } else return book;
+            })
+        }
+    }
 
     async logout() {
         await this.supabase?.auth.signOut();
